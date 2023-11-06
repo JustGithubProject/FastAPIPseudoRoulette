e25332a9c02d4067bfd3e44262e4c6e3c2e10f81
JustGithubProject <Kropivadanil@gmail.com>
2023-11-06 11:36:51 +0200

    Настройка базы данных

--- src/database.py
+++ src/database.py
@@ -5,6 +5,15 @@ from sqlalchemy.orm import sessionmaker
 from config import Config
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{Config.USERNAME}:{Config.PASSWORD}@{Config.HOST}/{Config.DB_NAME}"
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    except:
+        db.close()
